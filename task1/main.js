8f8f3b163ea1c84d2dfacb6aac0d72c04fc62a64
Podexus23 <santon23@gmail.com>
2021-10-13 11:29:41 +0300

    feat: add tables

--- task1/main.js
+++ task1/main.js
@@ -1,4 +1,5 @@
-function getDate(){
+//calculate actual date for footer
+function getDate () {
    let date = new Date();
    let year = date.getFullYear();
    let month = date.getMonth();
@@ -8,3 +9,44 @@ function getDate(){
    document.querySelector('.time').innerHTML = `${day}.${month}.${year}`;
 }
 setInterval(getDate, 0);
+
+//sort values by changing table class
+function sortingTable () {
+   let radioSort = document.querySelector('.table-sort')
+   let tableContainer = document.querySelector('.table-container')
+
+   radioSort.addEventListener('click', (e) => {
+      if(e.target == document.querySelector('#reversed')) {
+         tableContainer.setAttribute('class','table-container_reversed')
+         console.log('reversed')
+      }
+      if(e.target == document.querySelector('#normal')) {
+         tableContainer.setAttribute('class','table-container')
+         console.log('normal')
+      }
+   })
+}
+sortingTable();
+
+function coloriseTable() {
+   let radioColor = document.querySelector('.table-color')
+   let table = document.querySelector('.table-style')
+//document.elm.style.border = "3px solid #FF0000";
+   radioColor.addEventListener('click', (e) => {
+      if(e.target == document.querySelector('#dark')) {
+         table.removeAttribute('class')
+         table.setAttribute('class','man-table table-style_dark')
+         console.log('dark-color')
+      }
+      if(e.target == document.querySelector('#colored')) {
+         table.removeAttribute('class')
+         table.setAttribute('class','man-table table-style_colored')
+         console.log('colored-color')
+      }
+      if(e.target == document.querySelector('#default')) {
+         table.removeAttribute('class')
+         table.setAttribute('class','man-table table-style')
+         console.log('default-color')
+      }})
+}
+coloriseTable();
