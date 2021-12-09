d69091e89558cc75f7e533e9840732c9315c1703
Podexus23 <santon23@gmail.com>
2021-12-09 19:02:01 +0300

    feat: add result

--- task6/js/main.js
+++ task6/js/main.js
@@ -31,7 +31,7 @@ const pizzaBaseType = {
     price: 17,
   },
 }
-// продукты: перец помидоры курица колбасы ветчина грибы маслины ананас халапеньо
+// продукты:
 const pizzaProducts = {
   pepper: {
     calories: 10,
@@ -112,7 +112,12 @@ const sauce = {
     price: 3,
   },
 }
-
+//result
+const result = {
+  dough: 'default',
+  products: 'default',
+  sauce: 'default',
+}
 class Pizza {
   constructor(base, sauce, ...products) {
     this.base = base;
@@ -206,9 +211,9 @@ class Pizza {
 }
 Pizza.allPizzas = [];
 
-let margarita = new Pizza(pizzaBaseType.italian, sauce.cheese, pizzaProducts.ham, pizzaProducts.tomato)
-let cheapPizza = new Pizza(pizzaBaseType.thin, sauce.cheese, pizzaProducts.jalapeno);
-let luxuryPizza = new Pizza(pizzaBaseType.thick, sauce.cheese, pizzaProducts.chicken, pizzaProducts.ham)
+// let margarita = new Pizza(pizzaBaseType.italian, sauce.cheese, pizzaProducts.ham, pizzaProducts.tomato)
+// let cheapPizza = new Pizza(pizzaBaseType.thin, sauce.cheese, pizzaProducts.jalapeno);
+// let luxuryPizza = new Pizza(pizzaBaseType.thick, sauce.cheese, pizzaProducts.chicken, pizzaProducts.ham)
 
 // console.log(`%cBase price: ${margarita.basePrice()} and %cfinal price: ${margarita.madePizza()} of margarita`, 'color: #bada55', 'color: #c44');
 // console.log(`%cBase price: ${cheapPizza.basePrice()} and %cfinal price: ${cheapPizza.madePizza()} of cheapPizza`, 'color: #bada55', 'color: #c44');
@@ -217,84 +222,151 @@ let luxuryPizza = new Pizza(pizzaBaseType.thick, sauce.cheese, pizzaProducts.chi
 // console.log(luxuryPizza.myName())
 // console.log(luxuryPizza.ps())
 
+createPage();
+
 function createPage() {
-  createDoughBlock(pizzaBaseType);
-  createProductsBlock(pizzaProducts);
-  createSauceBlock(sauce);
+  createSelectionBlock("dough", pizzaBaseType)
+  createSelectionBlock("product", pizzaProducts)
+  createSelectionBlock("sauce", sauce)
+  createResultBlock('result', result)
 };
 
-function createDoughBlock(objType) {
+function createSelectionBlock(name, obj) {
   const article = document.createElement('article');
   const h2 = document.createElement('h2');
   const div = document.createElement('div');
-  const type = 'dough';
-  const names = Object.keys(objType);
-  article.classList.add('pizzamaker-type');
+  const names = Object.keys(obj);
+
+  article.classList.add(`pizzamaker-${name}`);
   article.append(h2);
   article.append(div)
 
   h2.classList.add('title');
-  h2.textContent = 'Select the type of dough';
+  h2.textContent = `Select the type of ${name}`;
 
   div.classList.add('wrapper');
-  div.classList.add('doughtype');
+  div.classList.add(`${name}`);
+
   names.forEach((elem) => {
-    div.append(createTypeBlock(type, elem))
+    div.append(createTypeBlock(name, elem))
   })
+
   document.querySelector('.pizzamaker').append(article)
 }
 
-function createSauceBlock(objType) {
+/* differences between usual createBlock */
+/* textContent, add option "empty" for createTypeBlock*/
+function createResultBlock(name, obj) {
   const article = document.createElement('article');
   const h2 = document.createElement('h2');
   const div = document.createElement('div');
-  const type = 'sauce';
-  const names = Object.keys(objType);
-  article.classList.add('pizzamaker-type');
+  const names = Object.keys(obj);
+
+  article.classList.add(`pizzamaker-${name}`);
   article.append(h2);
   article.append(div)
 
   h2.classList.add('title');
-  h2.textContent = 'Select the type of sauce';
+  h2.textContent = `Your pizza`;
 
   div.classList.add('wrapper');
-  div.classList.add('sauce');
+  div.classList.add(`${name}`);
+
   names.forEach((elem) => {
-    div.append(createTypeBlock(type, elem))
+    div.append(createTypeBlock(name, elem, 'empty'))
   })
+
   document.querySelector('.pizzamaker').append(article)
 }
 
-function createProductsBlock(objType) {
-  const article = document.createElement('article');
-  const h2 = document.createElement('h2');
-  const div = document.createElement('div');
-  const type = 'product';
-  const names = Object.keys(objType);
-  article.classList.add('pizzamaker-type');
-  article.append(h2);
-  article.append(div)
-
-  h2.classList.add('title');
-  h2.textContent = 'Select the products';
-
-  div.classList.add('wrapper');
-  div.classList.add('product');
-  names.forEach((elem) => {
-    div.append(createTypeBlock(type, elem))
-  })
-  document.querySelector('.pizzamaker').append(article)
-}
-
-function createTypeBlock(type, name) {
+function createTypeBlock(type, name, key = "default") {
   const div = document.createElement('div');
   const img = document.createElement('div');
+
   img.classList.add('block-img')
+
   div.classList.add(`${type}-${name}`)
   div.classList.add(`type-block`)
+  div.dataset.active = key;
   div.textContent = name;
   div.prepend(img);
   return div
 }
 
-createPage()
+function makePizza() {
+  selectBlock();
+  showResultBlock();
+  // ValidatePizza();
+}
+
+function selectBlock() {
+  const wrappers = document.querySelectorAll('.wrapper');
+
+  wrappers.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+      const blocks = elem.querySelectorAll('.type-block')
+      if (e.target.closest('.type-block')) {
+        let block = e.target.closest('.type-block');
+        if (block.dataset.active == "default") {
+          if (elem.classList.contains('product')) {} else {
+            blocks.forEach((elem) => {
+              elem.dataset.active = "default"
+            })
+          }
+          block.dataset.active = "active"
+        } else if (block.dataset.active == "active") {
+          block.dataset.active = "default";
+        }
+      }
+
+    })
+  })
+}
+
+function showResultBlock() {
+  const wrappers = document.querySelectorAll('.wrapper');
+  const doughResult = document.querySelector('.result-dough');
+  const productResult = document.querySelector('.result-products');
+  const sauceResult = document.querySelector('.result-sauce');
+
+  wrappers.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+      if (e.target.closest('.type-block') && elem.querySelector('.type-block[data-active="active"]')) {
+        let chosen = elem.querySelector('.type-block[data-active="active"]')
+        // console.dir(chosen)
+        if (chosen.parentElement.classList.contains('dough')) {
+          doughResult.dataset.active = 'load';
+          doughResult.innerHTML = chosen.innerHTML;
+        }
+        if (chosen.parentElement.classList.contains('sauce')) {
+          sauceResult.dataset.active = 'load';
+          sauceResult.innerHTML = chosen.innerHTML;
+        }
+        if (chosen.parentElement.classList.contains('product')) {
+
+          productResult.dataset.active = 'load';
+          productResult.innerHTML = ''
+          productResult.append(addToProductCart())
+          productResult.append('Products');
+        }
+      }
+    })
+  })
+}
+
+function addToProductCart() {
+  const products = document.querySelectorAll('.product .type-block');
+  console.dir(products)
+  const div = document.createElement('div');
+  div.classList.add('result-basket')
+  products.forEach((elem) => {
+    if (elem.dataset.active == 'active') {
+      let insider = createTypeBlock('result', elem.textContent, key = "load")
+      insider.innerHTML = elem.innerHTML;
+      div.append(insider)
+    }
+  })
+  return div
+}
+
+makePizza()
