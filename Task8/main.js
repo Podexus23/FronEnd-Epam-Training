13bcb9af2298e6d3758560549c257f9fbfea9cb8
Podexus23 <santon23@gmail.com>
2022-01-01 23:55:22 +0300

    feat: can save data ass .json file

--- Task8/main.js
+++ Task8/main.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('.edit-form');
-const ZAWARDO = 2000;
+const ZAWARDO = 5000;
 let order = new Array(3);
 //main class that revert JSON to object and can create a block in HTML
 class Reverted {
@@ -15,12 +15,13 @@ class Reverted {
   constructBlock(block) {
     block.innerHTML = `
     <div class="block-data">
-          <p class="data">Date: ${this.data}</p>
-          <p class="time">Time: ${this.time}</p>
+          <p class="data">Date: <span class="subinfo">${this.data}</span></p>
+          <p class="time">Time: <span class="subinfo">${this.time}</span></p>
     </div>
-        <p class="block-event-name">Event: ${this.title}</p>
-        <p class="block-priority">Priority: ${this.priority} </p>
-        <p class="block-notes">Notes: ${this.notes}</p>`
+        <p class="block-event-name">Event: <span class="subinfo">${this.title}</span></p>
+        <p class="block-priority">Priority: <span class="subinfo">${this.priority}</span> </p>
+        <p class="block-notes">Notes: <span class="subinfo">${this.notes}</span></p>
+    <a class="save-button">save</a>`
   }
 }
 //takes local info from .json files
@@ -34,25 +35,22 @@ async function fillBlocksWithLocal() {
   let counter = 1;
   const events = document.querySelectorAll('.event-block');
 
-  // console.log(order)
   for (let event of events) {
     let conductor;
     changeStatus(counter, "loading...")
     if (order[counter - 1]) {
-      console.log(order[counter - 1])
       conductor = order[counter - 1];
     } else {
       conductor = new Reverted(await takeInfo(`./json/your_name_data${counter}.json`))
     }
-    // let conductor = new Reverted(await takeInfo(`./json/your_name_data${counter}.json`))
     let time = new Promise((resolve) => {
       setTimeout(() => resolve(conductor), ZAWARDO)
     })
     let smth = await time;
-    // console.log(smth)
     smth.constructBlock(event)
     changeStatus(counter, "loaded")
-    counter++
+    counter++;
+    saveInfo();
   }
   counter = 1;
 }
@@ -66,7 +64,7 @@ function parseEditionForm() {
     priority: form.querySelector('#priority-event').value,
     notes: form.querySelector('#notes-event').value,
   }
-  return eventInfo
+  return JSON.stringify(eventInfo)
 }
 //search for checked radio and return it's number as text
 function checkedBlock(type) {
@@ -92,8 +90,7 @@ function checkedBlock(type) {
 //replacing info from form to chosen block
 const button = document.querySelector('.submit-button');
 button.addEventListener('click', () => {
-  let string = JSON.stringify(parseEditionForm())
-  let demo = new Reverted(string);
+  let demo = new Reverted(parseEditionForm());
 
   putInOrder(demo, checkedBlock('number'))
   formCleaner()
@@ -127,5 +124,32 @@ myNameIsDio()
 
 function putInOrder(eventClass, radioNumber) {
   order[radioNumber - 1] = eventClass;
-  console.log(eventClass, radioNumber)
+}
+
+function saveInfo() {
+  let saveButtons = document.querySelectorAll('.save-button');
+  saveButtons.forEach((elem) => {
+    elem.addEventListener('click', save)
+  })
+}
+
+function save(e) {
+  let father = e.target.parentNode;
+  let info = father.querySelectorAll('.subinfo');
+
+  let blockInfo = {
+    date: info[0].textContent,
+    time: info[1].textContent,
+    title: info[2].textContent,
+    priority: info[3].textContent,
+    notes: info[4].textContent,
+  }
+
+  let file = new Blob([JSON.stringify(blockInfo)], {
+    type: 'text/plain'
+  });
+  e.target.href = URL.createObjectURL(file);
+  if (father.dataset.number == 'first') e.target.download = `your_name_data1.json`;
+  if (father.dataset.number == 'second') e.target.download = `your_name_data2.json`;
+  if (father.dataset.number == 'third') e.target.download = `your_name_data3.json`;
 }
